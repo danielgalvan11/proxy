2c20a8b18c89d35b7810b12d87f9ff39fa8b6226
Daniel Galván <34138796+danielgalvan11@users.noreply.github.com>
2022-03-01 01:52:32 +0000

    Update revoke.js

--- revoke.js
+++ revoke.js
@@ -1,1 +1,18 @@
+var DIRECT = "DIRECT";
+var PROXY = "PROXY 127.0.0.1:8021";
 
+var ruler = [
+  
+];
+
+function FindProxyForURL(url, host) {
+  host = host.toLowerCase();
+  for(var i = 0; i < ruler.length; i++){
+    var copyright = ruler[i];
+    if(shExpMatch(host, "*" + copyright + "*")){
+      return PROXY;
+    }
+  }
+
+  return DIRECT;
+}
