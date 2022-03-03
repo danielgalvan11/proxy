a2799efcfd329f3b3832527fa47f9b70b1b218ac
Daniel Galván <34138796+danielgalvan11@users.noreply.github.com>
2022-03-02 23:19:04 -0400

    -.-

--- revoke.js
+++ revoke.js
@@ -2,7 +2,7 @@ var DIRECT = "DIRECT";
 var PROXY = "PROXY 127.0.0.1:8021";
 
 var ruler = [
- /*
+
  //apple
  "mesu",
  "ocsp",
@@ -92,7 +92,6 @@ var ruler = [
  "ezvizlife",
  "markmonitor",
 
-*/
 ];
 
 function FindProxyForURL(url, host) {
