92ea68547121b06d9b28601c288b7354920f14f8
Daniel Galván <34138796+danielgalvan11@users.noreply.github.com>
2024-09-07 14:03:40 -0400

    Update GalviBlock.js

--- GalviBlock.js
+++ GalviBlock.js
@@ -55,7 +55,7 @@ var ruler = [
 //"http://ppq.apple.com",
 //"http://certs.apple.com",
 
-	/*
+	
  //ads
  "applvn",
  "applovin",
@@ -81,7 +81,7 @@ var ruler = [
  "mxpnl",
  "admob",
  "parsely",
-	
+	/*
  "trafficjunky",
  "brightcove",
  "criterio",
